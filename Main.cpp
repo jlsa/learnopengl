5fc5f5d971d10bf637e32ae335252a9b57df8dc5
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-03-30 23:08:06 +0200

    fragmentshader code added

--- Main.cpp
+++ Main.cpp
@@ -82,9 +82,13 @@ int main()
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
 
 	int success;
 	char infoLog[512];
