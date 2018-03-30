66948912ab5872fe91bbe5d434ae293db29b09ea
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-03-30 23:05:30 +0200

    added fragmentshader and hardcoded shader sources

--- Main.cpp
+++ Main.cpp
@@ -13,6 +13,27 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char * GAME_TITLE = "Project-Mayra";
 
+// Shader sources
+const GLchar* vertexShaderSource = R"glsl(
+    #version 330 core
+	layout (location = 0) in vec3 aPos;
+
+	void main()
+	{
+		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	}
+)glsl";
+
+const GLchar* fragmentShaderSource = R"glsl(
+    #version 330 core
+	out vec4 FragColor;
+
+	void main()
+	{
+		FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	}
+)glsl";
+
 int main()
 {
 	// glfw: initialize and configure
