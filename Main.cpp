0fd0d0cc333c2957611854bfcd3bd74958da52a3
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-03-31 09:24:34 +0200

    Element buffer object added and not its a quad

--- Main.cpp
+++ Main.cpp
@@ -121,23 +121,41 @@ int main()
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
-	float vertices[] = {
+	/*float vertices[] = {
 		// X	 Y	   Z
 		-0.5f, -0.5f, 0.0f, // vertex 1 (left)
 		 0.5f, -0.5f, 0.0f, // vertex 2 (right)
 		 0.0f,  0.5f, 0.0f  // vertex 3 (top)
+	};*/
+
+	float vertices[] = {
+		// X	 Y	   Z
+		 0.5f,  0.5f, 0.0f, // top right
+		 0.5f, -0.5f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f	// top left
 	};
 
-	unsigned int VBO, VAO;
+	unsigned int indices[] = { // note that we start from 0!
+		0, 1, 3,	// first triangle
+		1, 2, 3		// second triangle
+	};
+
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
+	
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -152,7 +170,7 @@ int main()
 
 
 	// uncomment this call to draw in wireframe polygons.
-	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
 	// render loop
@@ -171,7 +189,9 @@ int main()
 		// draw our first triangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		// glBindVertexArray(0); // no need to unbind every time
 
 
