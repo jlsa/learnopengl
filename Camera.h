b16934956ae03aecf71ef0d828bbbf103b0df2d3
Joel Hoekstra <jlsahoekstra@gmail.com>
2018-04-18 19:05:47 +0200

    Excersise: Try to create your own LookAt function where you manually create a view matrix as discussed at the start of this tutorial. Replace glm's LookAt function with your own implementation and see if it still acts the same. (did not do this).. Have to come back to the math part later.

--- Camera.h
+++ Camera.h
@@ -135,7 +135,38 @@ private:
 
 	glm::mat4 GetLookAt(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
 	{
-		return glm::lookAt(position, target, worldUp);
+		// 1: position
+		
+		// 2: calculate cameraDirection
+		glm::vec3 zaxis{ glm::normalize(position - target) };
+
+		// 3: get positive right axis vector
+		glm::vec3 xaxis{ glm::normalize(glm::cross(glm::normalize(worldUp), zaxis)) };
+
+		// 4: calculate camera up vector
+		glm::vec3 yaxis{ glm::cross(zaxis, xaxis) };
+
+		// create translation and rotation matrix
+		// in glm we access elements as mat[col][row] due to column-major layout #TODO learn what this means.
+		glm::mat4 translation; // identity matrix by default
+		translation[3][0] = -position.x;
+		translation[3][1] = -position.y;
+		translation[3][2] = -position.z;
+
+		glm::mat4 rotation;
+		rotation[0][0] = xaxis.x; // first column first row
+		rotation[1][0] = xaxis.y;
+		rotation[2][0] = xaxis.z;
+		rotation[0][1] = yaxis.x; // first column second row
+		rotation[1][1] = yaxis.y;
+		rotation[2][1] = yaxis.z;
+		rotation[0][2] = zaxis.x; // first column third row
+		rotation[1][2] = zaxis.y;
+		rotation[2][2] = zaxis.z;
+
+		// return lookAt matrix combination of translation and rotation matrix
+		return rotation * translation; // Remember to read from right to left (first translation then rotation)
+		//return glm::lookAt(position, target, worldUp);
 	}
 };
 #endif
